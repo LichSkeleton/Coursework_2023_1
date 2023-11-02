c84214f952356a17dd7db6c28cff6d26f8d03a44
LichSkeleton <lichskeleton1@gmail.com>
2023-11-02 20:50:20 +0200

    SinglePost

--- server.js
+++ server.js
@@ -17,26 +17,72 @@ app.get('/', (req, res)=> {
 })
 
 app.get('/courses', (req, res)=>{
-    const sql = "SELECT * FROM courses";
-    db.query(sql, (err,data)=> {
-        if(err) {
-            console.error(err); // Log the error to the server console
-            return res.status(500).json({ error: "Internal server error" }); // Return an error response
-        }
-        return res.json(data);
-    })
-})
+    const { id } = req.query; // Get the 'id' query parameter
+
+    if (id) {
+        // If 'id' is provided, fetch a single course by ID
+        const sql = `SELECT * FROM courses WHERE id = ${id}`;
+        db.query(sql, (err, data)=> {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+            return res.json(data[0]); // Assuming 'id' is unique, return the first result
+        });
+    } else {
+        // If 'id' is not provided, fetch all courses
+        const sql = "SELECT * FROM courses";
+        db.query(sql, (err, data)=> {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+            return res.json(data);
+        });
+    }
+});
+
+app.get('/coursesvideos', (req, res)=>{
+    const { id } = req.query; // Get the 'id' query parameter
+
+    if (id) {
+        // If 'id' is provided, fetch a single coursesvideos by ID
+        const sql = `SELECT * FROM courses_videos WHERE course_id = ${id}`;
+        db.query(sql, (err, data)=> {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+            return res.json(data);
+        });
+    }else return res.status(500).json({ error: "Data wasn't fetched" });
+});
 
 app.get('/authors', (req, res)=>{
-    const sql = "SELECT * FROM authors";
-    db.query(sql, (err,data)=> {
-        if(err) {
-            console.error(err); // Log the error to the server console
-            return res.status(500).json({ error: "Internal server error" }); // Return an error response
-        }
-        return res.json(data);
-    })
-})
+    const { id } = req.query; // Get the 'id' query parameter
+
+    if (id) {
+        // If 'id' is provided, fetch a single author by ID
+        const sql = `SELECT * FROM authors WHERE id = ${id}`;
+        db.query(sql, (err, data)=> {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+            return res.json(data[0]); // Assuming 'id' is unique, return the first result
+        });
+    } else {
+        // If 'id' is not provided, fetch all authors
+        const sql = "SELECT * FROM authors";
+        db.query(sql, (err, data)=> {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+            return res.json(data);
+        });
+    }
+});
 
 
 app.get('/categories', (req, res)=>{
